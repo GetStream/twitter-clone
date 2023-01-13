5ee3bfbfad544df5607056e637a972d5c352c976
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-01-13 10:35:31 +0100

    Add DB storage and password check.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
 
+var db = require('../db');
+
 require('dotenv').config();
 
 const api_key = process.env.STREAM_API_KEY;
@@ -11,59 +13,61 @@ const app_id = process.env.STREAM_APP_ID;
 
 const signup = async (req, res) => {
     try {
-        const { fullName, username, password, phoneNumber } = req.body;
+        const { username, password } = req.body;
 
         const userId = crypto.randomBytes(16).toString('hex');
-        
-        const feedClient = connect(api_key, api_secret, app_id, { location: 'eu-west' });
-        const chatClient = StreamChat.getInstance(api_key, api_secret);//, location = "eu-west");
+        register(userId, username, password, function(err, result) {
+            if (err) {
+                res.status(500).json({ message: err });
+                return;
+            }
+            const feedClient = connect(api_key, api_secret, app_id, { location: 'eu-west' });
+            const chatClient = StreamChat.getInstance(api_key, api_secret);//, location = "eu-west");
+                
+            const chatResult = chatClient.upsertUser({
+                id: userId,
+                username: username,
+                // email: user.email,
+                // image: user.photoURL,
+              });
+    
+            const feedResult = feedClient.user(userId).getOrCreate();
+            const feedToken = feedClient.createUserToken(userId);
+            const chatToken = chatClient.createToken(userId);
+    
+            res.status(200).json({ feedToken, chatToken, username, userId });
+        })
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        
-        const chatResult = await chatClient.upsertUser({
-            id: userId,
-            name: fullName,
-            username: username,
-            // email: user.email,
-            // image: user.photoURL,
-          });
+    } catch (err) {
+        console.log(err);
 
-        const feedResult = await feedClient.user(userId).getOrCreate({
-              name: fullName
-          });
-        const feedToken = feedClient.createUserToken(userId);
-        const chatToken = chatClient.createToken(userId);
-
-        res.status(200).json({ chatToken, fullName, username, userId, hashedPassword, phoneNumber });
-    } catch (error) {
-        console.log(error);
-
-        res.status(500).json({ message: error });
+        res.status(500).json({ message: err });
     }
 };
 
 const login = async (req, res) => {
     try {
         const { username, password } = req.body;
+
+        verify(username, password, async function(err, result) {
+            if (err) {
+                res.status(500).json({ message: err });
+                return;
+            }
+
+            const feedClient = connect(api_key, api_secret, app_id, { location: 'eu-west' });
+            const chatClient = StreamChat.getInstance(api_key, api_secret);//, location="eu-west" )
+
+            const { users } = await chatClient.queryUsers({ username: { $eq: username } });
+            if(!users.length) return res.status(400).json({ message: 'User not found' });
+
+            const feedResult = feedClient.user(users[0].id).get()
+            const chatToken = chatClient.createToken(username);
+            const feedToken = feedClient.createUserToken(username);
+
+            res.status(200).json({ feedToken, chatToken, username, userId: users[0].id});
+        })
         
-        const feedClient = connect(api_key, api_secret, app_id, { location: 'eu-west' });
-        const chatClient = StreamChat.getInstance(api_key, api_secret);//, location="eu-west" )
-
-        const { users } = await chatClient.queryUsers({ username: { $eq: username } });
-        if(!users.length) return res.status(400).json({ message: 'User not found' });
-
-        const feedResult = await feedClient.user(users[0].id).get()
-        const chatToken = await chatClient.createToken(username);
-        const feedToken = await feedClient.createUserToken(username);
-
-        // TODO: We need storage and a proper password check.
-        const success = true;//await bcrypt.compare(password, users[0].hashedPassword);
-
-        if(success) {
-            res.status(200).json({ feedToken, chatToken, fullName: users[0].fullName, username, userId: users[0].id});
-        } else {
-            res.status(500).json({ message: 'Incorrect password' });
-        }
     } catch (error) {
         console.log(error);
 
@@ -71,4 +75,41 @@ const login = async (req, res) => {
     }
 };
 
+function register(userId, username, password, cb) {
+    var salt = bcrypt.genSaltSync(10);
+
+    var data = {
+        userId: userId,
+        username: username,
+        hashed_password: bcrypt.hashSync(password, salt),
+        salt: salt
+    }
+
+    var sql ='INSERT INTO users (id, username, hashed_password, salt) VALUES (?,?,?,?)'
+    var params =[data.userId, data.username, data.hashed_password, data.salt]
+    db.run(sql, params, function (err, innerResult) {
+        if (err){
+            cb(err)
+        } else {
+            cb(null, data)
+        }
+    });
+}
+
+function verify(username, password, cb) {
+    db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+      if (err) { return cb(err); }
+      if (!row) { return cb('Incorrect username or password.'); }
+      
+      const hashed_password = bcrypt.hashSync(password, row.salt);
+
+      if (hashed_password === row.hashed_password) {
+        cb(null, row);        
+      } else {
+        return cb('Incorrect username or password.');
+      }
+    });
+  }
+// "$2b$10$Z2LS.rJyA7u9kKAdtr3zQ.QCOaiYKiLpgisCDrzOBjjh6lInQGZQS"
+// "$2b$10$Z2LS.rJyA7u9kKAdtr3zQ.QCOaiYKiLpgisCDrzOBjjh6lInQGZQS"
 module.exports = { signup, login }
---
+++ server/db.js
@@ -0,0 +1,27 @@
+var sqlite3 = require('sqlite3');
+var mkdirp = require('mkdirp');
+var crypto = require('crypto');
+
+mkdirp.sync('./var/db');
+
+var db = new sqlite3.Database('./var/db/twitter-clone.db');
+
+db.serialize(function() {
+  // create the database schema for the todos app
+  db.run("CREATE TABLE IF NOT EXISTS users ( \
+    id TEXT PRIMARY KEY, \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB \
+  )");
+  
+  // create an initial user (username: alice, password: letmein)
+  var salt = crypto.randomBytes(16);
+  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+    'alice',
+    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
+    salt
+  ]);
+});
+
+module.exports = db;
