e0aefb6b2cd4a7c77de4265d30af2a67576ba11c
khushal87 <khushal.agarwal987@gmail.com>
2023-01-15 10:46:35 +0530

    refactor: code format through linting, simiplify controller logic to utils, fix token through userId

--- server/db.js
+++ server/db.js
@@ -1,27 +1,39 @@
-var sqlite3 = require('sqlite3');
-var mkdirp = require('mkdirp');
-var crypto = require('crypto');
+const sqlite3 = require("sqlite3");
+const mkdirp = require("mkdirp");
+const crypto = require("crypto");
 
-mkdirp.sync('./var/db');
+mkdirp.sync("./var/db");
 
-var db = new sqlite3.Database('./var/db/twitter-clone.db');
+const db = new sqlite3.Database("./var/db/twitter-clone.db");
 
-db.serialize(function() {
-  // create the database schema for the todos app
-  db.run("CREATE TABLE IF NOT EXISTS users ( \
-    id TEXT PRIMARY KEY, \
-    username TEXT UNIQUE, \
-    hashed_password BLOB, \
-    salt BLOB \
-  )");
-  
-  // create an initial user (username: alice, password: letmein)
-  var salt = crypto.randomBytes(16);
-  db.run('INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
-    'alice',
-    crypto.pbkdf2Sync('letmein', salt, 310000, 32, 'sha256'),
-    salt
-  ]);
+// create the database schema for the todos app
+const createTable = () => {
+    db.run(
+        "CREATE TABLE IF NOT EXISTS users ( \
+id TEXT PRIMARY KEY, \
+username TEXT UNIQUE, \
+hashed_password BLOB, \
+salt BLOB \
+)"
+    );
+};
+
+// create an initial user (username: alice, password: letmein)
+const createInitialUser = () => {
+    const salt = crypto.randomBytes(16);
+    db.run(
+        "INSERT OR IGNORE INTO users (username, hashed_password, salt) VALUES (?, ?, ?)",
+        [
+            "alice",
+            crypto.pbkdf2Sync("letmein", salt, 310000, 32, "sha256"),
+            salt,
+        ]
+    );
+};
+
+db.serialize(function () {
+    createTable();
+    createInitialUser();
 });
 
 module.exports = db;
