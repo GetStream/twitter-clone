7aa1530374f4900d47fe0d4212c9641908da32f0
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-02-14 12:21:10 +0100

    Add async extension to ChatClient

---
+++ TwitterClone/Targets/Chat/Sources/ChatClient+createCall.swift
@@ -0,0 +1,19 @@
+//
+//  ChatClient+createCall.swift
+//  Chat
+//
+//  Created by Jeroen Leenarts on 14/02/2023.
+//  Copyright © 2023 Stream.io Inc. All rights reserved.
+//
+
+import StreamChat
+
+public extension ChatClient {
+    public func createCall(with id: String, in channelId: ChannelId) async throws -> CallWithToken {
+        try await withCheckedThrowingContinuation({ continuation in
+            channelController(for: channelId).createCall(id: id, type: "video") { result in
+                continuation.resume(with: result)
+            }
+        })
+    }
+}
