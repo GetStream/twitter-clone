5cba7a775d43fced0ce70dbfced168481ccba3a6
Stefan <stefan.blos@gmail.com>
2023-03-01 10:25:49 +0100

    Re-work imageFromURL to show placeholder

--- TwitterClone/Targets/Spaces/Sources/View/ImageFromUrl.swift
+++ TwitterClone/Targets/Spaces/Sources/View/ImageFromUrl.swift
@@ -15,16 +15,25 @@ struct ImageFromUrl: View {
     var offset: CGFloat = 0
     
     var body: some View {
-        AsyncImage(url: url) { image in
-            image.resizable()
-                .frame(width: size, height: size)
-                .aspectRatio(contentMode: .fit)
-                .clipShape(Circle())
-                .offset(x: offset, y: offset)
-        } placeholder: {
-            ProgressView()
-                .frame(width: size, height: size)
-                .offset(x: offset, y: offset)
+        AsyncImage(url: url) { phase in
+            if let image = phase.image {
+                image.resizable()
+                    .frame(width: size, height: size)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .offset(x: offset, y: offset)
+            } else if phase.error != nil || url == nil {
+                Image(systemName: "person.crop.circle.fill")
+                    .resizable()
+                    .frame(width: size, height: size)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                    .offset(x: offset, y: offset)
+            } else {
+                ProgressView()
+                    .frame(width: size, height: size)
+                    .offset(x: offset, y: offset)
+            }
         }
     }
 }
@@ -33,7 +42,20 @@ struct ImageFromUrl_Previews: PreviewProvider {
     static var previews: some View {
         ImageFromUrl(
             url: URL(string: "https://getstream.io/static/237f45f28690696ad8fff92726f45106/c59de/thierry.webp"),
-            size: 40
+            size: 80
         )
+        .previewDisplayName("Valid URL")
+        
+        ImageFromUrl(
+            url: URL(string: "https://getstream.io/static/237f45f28690696ad8fff92726f45106/c59de/blablabla.webp"),
+            size: 80
+        )
+        .previewDisplayName("Invalid URL")
+        
+        ImageFromUrl(
+            url: nil,
+            size: 80
+        )
+        .previewDisplayName("Nil URL")
     }
 }
