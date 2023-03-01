2b4125805060ecf26d63ddd9e15334b7066404e8
Stefan <stefan.blos@gmail.com>
2023-03-01 12:24:23 +0100

    Streamlined Space related code

--- TwitterClone/Targets/Spaces/Sources/ViewModel/SpacesViewModel+Space.swift
+++ TwitterClone/Targets/Spaces/Sources/ViewModel/SpacesViewModel+Space.swift
@@ -14,15 +14,17 @@ extension SpacesViewModel {
     func joinSpace(id: String) async {
         do {
             let channelId = try ChannelId(cid: "livestream:\(id)")
-            
-            // add user to the channel members
             let controller = chatClient.channelController(for: channelId)
+            
+            /// Add the user to the members of the channel.
             if let currentUserId = chatClient.currentUserId {
                 controller.addMembers(userIds: [currentUserId])
             }
             
+            /// Join the 100ms call with the spaceId in `id` and the `channelId`.
             await joinCall(with: id, in: channelId)
             
+            /// If everything worked, updated the state
             isInSpace = true
         } catch {
             setInfoMessage(text: error.localizedDescription, type: .error)
@@ -33,16 +35,24 @@ extension SpacesViewModel {
     
     @MainActor
     func leaveSpace(id: String) {
-        if let channelId = try? ChannelId(cid: "livestream:\(id)") {
+        do {
+            let channelId = try ChannelId(cid: "livestream:\(id)")
             let controller = chatClient.channelController(for: channelId)
             
+            /// Remove user from the channel members.
             if let currentUserId = chatClient.currentUserId {
                 controller.removeMembers(userIds: [currentUserId])
             }
+            
+            /// Leave the 100ms call.
+            leaveCall(with: id)
+            
+            /// If everything worked, update the state.
+            isInSpace = false
+        } catch {
+            setInfoMessage(text: error.localizedDescription, type: .error)
+            print(error.localizedDescription)
         }
-        
-//        leaveCall(with: id)
-        isInSpace = false
     }
     
     @MainActor
@@ -50,10 +60,15 @@ extension SpacesViewModel {
         do {
             let channelId = try ChannelId(cid: "livestream:\(id)")
             
+            /// Start a call with the space id as the name of the room and the `channelId`.
             let callId = await startCall(with: id, in: channelId)
             
+            /// Update the channel to have state running and the callId attached to its extraData.
             updateChannel(with: channelId, to: .running, callId: callId)
+            
+            /// If everything worked, update the state.
             isInSpace = true
+            setInfoMessage(text: "Your space has started.", type: .information)
         } catch {
             setInfoMessage(text: error.localizedDescription, type: .error)
             print(error.localizedDescription)
@@ -63,14 +78,22 @@ extension SpacesViewModel {
     
     @MainActor
     func endSpace(with id: String) {
-        if let channelId = try? ChannelId(cid: "livestream:\(id)") {
-            // TODO: This will need to be .finished, only for debugging purposes.
+        do {
+            let channelId = try ChannelId(cid: "livestream:\(id)")
+            
+            /// This channel will be updated to have the state of `.finished`.
+            /// TODO: This will need to be .finished, only for debugging purposes is it currently .planned.
             updateChannel(with: channelId, to: .planned)
+            
+            /// End the 100ms call.
+            endCall()
+            
+            /// If everything worked, update the state.
+            isInSpace = false
+        } catch {
+            setInfoMessage(text: error.localizedDescription, type: .error)
+            print(error.localizedDescription)
         }
-        // TODO: stop observing channel updates
-        // TODO: should we lock the room?
-        // TODO: deactivate to focus on channel updates for now
-//        endCall()
     }
     
 }
